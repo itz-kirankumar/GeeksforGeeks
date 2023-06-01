4d1ec07adf10f870198226f704261bf8c5fa9b54
Kiran Kumar G <96608447+itz-kirankumar@users.noreply.github.com>
2023-06-01 21:06:01 +0530

    Topological Sort - Solution

---
+++ Topological Sort/Solution.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+ public:
+ //Function to return list containing vertices in Topological order. 
+ vector<int> topoSort(int V, vector<int> adj[]) 
+ {
+     vector<int> indegree(V, 0);
+     
+     for(int i=0; i<V; i++){
+         for(auto it : adj[i]){
+             indegree[it]++;
+         }
+     }
+     vector<int> ans;
+     queue<int> q;
+     
+     vector<int> vis(V, 0);
+     
+     for(int i=0; i<V; i++){
+         if(indegree[i] == 0){
+             q.push(i);
+         }
+     }
+     while(!q.empty()){
+         int a = q.front();
+         q.pop();
+         ans.push_back(a);
+         
+         for(auto it: adj[a]){
+             indegree[it]--;
+             if(indegree[it] == 0) q.push(it);
+         }
+     }
+     return ans;
+ }
+};
